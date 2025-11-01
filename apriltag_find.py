272fedffe17c1967605b6adab1cc8ec39af1c2bd
Ben Ricket <ben.ricket@gmail.com>
2025-11-01 19:47:23 -0400

    Correct realsense camera intrinsics

--- apriltag_find.py
+++ apriltag_find.py
@@ -2,14 +2,18 @@ import cv2
 import sys
 import numpy as np
 
-RS_INTRINSIC_COLOR_1920 = np.array([
-    [1384.23,0,939.51],[0,1384.52,548.93],[0,0,1]
+# Parameters for Realsense Color channel, 
+RS_INTRINSIC_COLOR_640 = np.array([
+    [615.21,0,310.90],[0,614.45,243.97],[0,0,1]
 ])
 
-RS_DIST_COLOR_1920 = np.array([0,0,0,0,0])
+RS_DIST_COLOR_640 = np.array([0,0,0,0,0])
 
-def main():
-    cap = cv2.VideoCapture(0)
+def main(channel):
+    cap = cv2.VideoCapture(int(channel))
+    #cap.set(cv2.CAP_PROP_FRAME_WIDTH,640)
+    #w  = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    #print(f"w: {w}")
     if not cap.isOpened():
         print("Could not open video")
         sys.exit()
@@ -25,7 +29,7 @@ def main():
         corners = []
         ids = cv2.UMat()
         corners, ids, rejected = detector.detectMarkers(frame)
-        print(f"corners: {corners}, ids: {ids}")
+        #print(f"corners: {corners}, ids: {ids}")
 
         marker_length = 100 # 100 mm
         obj_pt_arr = np.asarray([
@@ -35,16 +39,18 @@ def main():
             [-marker_length/2.0,-marker_length/2.0,0]
         ])
         obj_points = cv2.Mat(obj_pt_arr)
-        camMatrix = RS_INTRINSIC_COLOR_1920
-        distCoeffs = RS_DIST_COLOR_1920
+        camMatrix = RS_INTRINSIC_COLOR_640
+        distCoeffs = RS_DIST_COLOR_640
 
         img_clone = frame
+        has_found_tag = False
         if len(corners) > 0: # if we found a marker
+            has_found_tag = True
             n_markers = len(corners[0])
-            print(n_markers)
+            #print(n_markers)
 
             for i in range(n_markers):
-                print("solving pnp")
+                #print("solving pnp")
                 _, rvecs, tvecs = cv2.solvePnP(
                     objectPoints=obj_pt_arr,
                     imagePoints=corners[i],
@@ -53,8 +59,8 @@ def main():
                 )
 
             img_clone = cv2.aruco.drawDetectedMarkers(img_clone,corners,ids)
-            print(f"rvec: {rvecs}")
-            print(f"tvec: {tvecs}")
+            #print(f"rvec: {rvecs}")
+            #print(f"X: {tvecs[0]*0.001} m, \nY: {tvecs[1]*0.001} m, \nZ: {tvecs[2]*0.001} m")
 
             for i in range(len(ids)):
                 cv2.drawFrameAxes(image=img_clone,
@@ -62,13 +68,23 @@ def main():
                     distCoeffs=distCoeffs,
                     rvec=rvecs,
                     tvec=tvecs,
-                    length=1.0,
-                    thickness=1
+                    length=100.0,
+                    thickness=10
                 )
 
         cv2.imshow("Camera Feed",img_clone)
+        if cv2.waitKey(1) & 0xFF == ord('p'):
+            # Print pose relative to camera
+            if has_found_tag:
+                print(f"X: {tvecs[0]*0.001} m, \nY: {tvecs[1]*0.001} m, \nZ: {tvecs[2]*0.001} m")
+                # X right, Y down, Z forward
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv)> 1:
+        channel = sys.argv[1]
+    else:
+        channel = 8 # realsense RGB color channel by default on my computer
+    main(channel=channel)
